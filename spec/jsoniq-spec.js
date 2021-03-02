cfcb976fa7477b9458833e3afddad2e4808044c1
Alhadis <gardnerjohng@gmail.com>
2021-03-03 09:47:16 +1100

    Rename JSONiq grammar scope to `source.jsoniq`
    The name `source.jq` conflicts with another format, one also named "JQ".
    Having both grammars installed (or registered within the same namespace)
    causes problems for both Atom users and GitHub's Linguist library, which
    uses this repository for JSONiq highlighting on GitHub.
    
    References: github/linguist#5233

--- spec/jsoniq-spec.js
+++ spec/jsoniq-spec.js
@@ -9,12 +9,12 @@ describe('JSONiq Grammar', function() {
             return atom.packages.activatePackage('language-jsoniq');
         });
         return runs(function() {
-            return grammar = atom.grammars.grammarForScopeName('source.jq');
+            return grammar = atom.grammars.grammarForScopeName('source.jsoniq');
         });
     });
 
     it('parses the grammar', function() {
         expect(grammar).toBeTruthy();
-        return expect(grammar.scopeName).toBe('source.jq');
+        return expect(grammar.scopeName).toBe('source.jsoniq');
     });
 });
